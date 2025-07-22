d090ab043f866e30b20e3f9c01459985eed189d5
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-22 19:29:44 -0300

    Programatic Navigation

--- recipe-book/src/views/AddRecipeView.vue
+++ recipe-book/src/views/AddRecipeView.vue
@@ -1,3 +1,24 @@
 <template>
   <div>Add form</div>
+  <form @submit.prevent="addRecipe">
+    <button type="submit">
+      Add
+    </button>
+  </form>
 </template>
+
+<script lang="ts" setup>
+import {useRouter} from 'vue-router';
+
+const router = useRouter()
+
+const addRecipe =()=>{
+  router.push({
+    name:'recipe',
+    params:{
+      id: 1 //change this later
+    }
+  })
+}
+
+</script>
