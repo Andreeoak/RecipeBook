2de2212139fb80829d82e6012014ca053f8d1cd4
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-22 18:50:39 -0300

    Adding recipies 1 and 2

--- recipe-book/src/views/HomeView.vue
+++ recipe-book/src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-
+  import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  Home
+  <main>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink :to="{name:'recipe', params: {id:1}}">"Recipe1"</RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{name:'recipe', params: {id:2}}">"Recipe2"</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
