3967ba350e4ecab89be45a4bdefdaf522076d585
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-22 20:03:24 -0300

    Data fetching on mount and watch

--- recipe-book/src/views/RecipeView.vue
+++ recipe-book/src/views/RecipeView.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    Recipe {{ $route.params.id }}
+    <div>
+      Recipe {{ $route.params.id }}
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink :to="{ name: 'recipe', params: { id: 1 } }">Recipe 1</RouterLink>
+        </li>
+        <li>
+          <RouterLink :to="{ name: 'recipe', params: { id: 3 } }">Recipe 3</RouterLink>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script setup lang="ts">
-import {useRoute} from 'vue-router';
+import { onMounted, watch } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
 
 const route = useRoute();
 
-console.log(route.params.id)
+console.log(route.params.id);
 
+onMounted(() => {
+  console.log('Fetching data inside mounted DOM');
+});
+
+watch(() => route.params.id, () => {
+  console.log('Fetching inside the watch');
+});
 </script>
