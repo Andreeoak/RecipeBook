8bf3c8f59e9dc291940db2e61be4c33b1ac9df57
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-25 19:00:49 -0300

    Using pinia to access local storage

--- recipe-book/src/main.ts
+++ recipe-book/src/main.ts
@@ -3,12 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import localStoragePlugin from '@/plugins/pinia/localStoragePlugin'
 
 const app = createApp(App)
 
-app.use(createPinia())
+
+const pinia= createPinia()
+pinia.use(localStoragePlugin)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
