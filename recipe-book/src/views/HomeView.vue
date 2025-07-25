658d80309b928da407f5ba55c61cd7bf99b8635a
Andre Pereira de Carvalho <andreesoul@gmail.com>
2025-07-25 18:08:56 -0300

    Search feature in homeview

--- recipe-book/src/views/HomeView.vue
+++ recipe-book/src/views/HomeView.vue
@@ -1,21 +1,37 @@
 <template>
   <main>
-    <nav>
+    <h1>Recipe Book</h1>
+    <div>
+      <input type="text" placeholder="Search for a recipe" v-model="searchRecipe"></input>
+    </div>
+
+    <nav v-if="filteredRecipes.length>0">
       <ul>
-        <li>
-          <RouterLink :to="{name:'recipe', params: {id:1}}">"Recipe1"</RouterLink>
-        </li>
-        <li>
-          <RouterLink :to="{name:'recipe', params: {id:2}}">"Recipe2"</RouterLink>
+        <li v-for="recipe in filteredRecipes" :key="recipe.id">
+          <RouterLink :to="{name:'recipe', params: {id:recipe.id}}">{{ recipe.name }}</RouterLink>
         </li>
       </ul>
     </nav>
+    <div v-else>
+      No recipes found
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
   import { useRecipeStore } from '@/stores/recipe';
+  import { ref, computed } from 'vue';
   import { RouterLink } from 'vue-router';
+
   const recipeStore = useRecipeStore();
+  const searchRecipe = ref('')
+
+  const filteredRecipes = computed(
+    ()=> recipeStore.recipes.filter(
+      recipe=>recipe.name.toLocaleLowerCase().includes(
+        searchRecipe.value.toLocaleLowerCase()
+      )
+    )
+  )
 </script>
 
